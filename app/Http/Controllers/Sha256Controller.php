57c018d5c3949558abd1151bc63fe33f2a690439
Moose <moosecodes@gmail.com>
2021-05-08 20:38:07 -0700

    Create hash and unhash functions for AES-256-CBC

--- app/Http/Controllers/Sha256Controller.php
+++ app/Http/Controllers/Sha256Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class Sha256Controller extends Controller
 {
@@ -11,9 +13,45 @@ class Sha256Controller extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
+     * 
+     * @author Moose <moosecodes@gmail.com>
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): string
     {
-        return 'Sha256Controller';
+        if (!$request->token) {
+            return 'No token provided. Please provide a string to encrypt.';
+        }
+        if ($request->decrypt) {
+            return $this->unHashString($request->token);
+        }
+        return $this->hashString($request->token);
+    }
+
+    /**
+     * Encrypts strings using AES-256-CBC
+     *
+     * @param string $token
+     * @return string
+     * 
+     * @author Moose <moosecodes@gmail.com>
+     */
+    protected function hashString($token): string {
+        return $encryptedValue = Crypt::encryptString($token);
+    }
+
+    /**
+     * Decrypts strings encoded with AES-256-CBC
+     *
+     * @param string $encryptedValue
+     * @return string
+     * 
+     * @author Moose <moosecodes@gmail.com>
+     */
+    protected function unHashString($encryptedValue): string {
+        try {
+            $decrypted = Crypt::decryptString($encryptedValue);
+        } catch (DecryptException $e) {}
+
+        return $decrypted;
     }
 }
